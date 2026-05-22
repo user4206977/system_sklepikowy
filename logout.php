4e587d9009691a423c1790407ae5a8c70994d9a2
Wojciech S <wojciechstrzezik@gmail.com>
2026-05-22 12:47:35 +0200

    Poprawa błędu w logout.php

--- logout.php
+++ logout.php
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 <?php
 session_start();
 session_destroy();
 header("Location: index.php");
-=======
-<?php
-session_start();
-session_destroy();
-header("Location: index.php");
->>>>>>> f3bacde68847ebeb5b938443835ccc6f47c1ac64
 exit;
