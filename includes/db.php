26f7abbd0de1802b57885cb5fa8df81b09accaef
Wojciech S <wojciechstrzezik@gmail.com>
2026-05-22 12:49:30 +0200

    Poprawa błędów w db.php'

--- includes/db.php
+++ includes/db.php
@@ -12,19 +12,4 @@ try {
 } catch (PDOException $e) {
     die("Błąd połączenia z bazą danych: " . $e->getMessage());
 }
-=======
-<?php
-// includes/db.php
-$host = 'localhost';
-$dbname = 'zegowska_szama';
-$user = 'root';
-$pass = '';
-
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Błąd połączenia z bazą danych: " . $e->getMessage());
-}
->>>>>>> f3bacde68847ebeb5b938443835ccc6f47c1ac64
 ?>
