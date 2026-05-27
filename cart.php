1f5af6b40a45fba72d928448ccf904608e2d7a0b
Wojciech Z <wojtekzlonkiewicz@gmail.com>
2026-05-27 09:50:02 +0100

    Dodanie formularza zamówień do cart.php

--- cart.php
+++ cart.php
@@ -4,3 +4,7 @@
 
 <div class="card p-3 shadow-sm">
     <h4>Suma: <span id="totalPrice">0.00</span> zł</h4>
+    <form id="orderForm" method="POST">
+        <input type="hidden" name="cart_data" id="cartDataInput">
+        <button type="button" onclick="submitOrder()" class="btn btn-success btn-lg w-100 mt-3">Złóż zamówienie i odbierz w sklepiku</button>
+    </form>
