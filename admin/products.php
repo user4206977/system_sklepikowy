f5d420329fc13777310872c26d6d1459d85d033c
Wojciech S <wojciechstrzezik@gmail.com>
2026-05-29 11:40:20 +0200

    Dodanie zabezpieczenia przeciw wejściu bez autoryzacji

--- admin/products.php
+++ admin/products.php
@@ -1,3 +1,7 @@
 <?php
 require_once '../includes/db.php';
 session_start();
+
+if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
+    die("<div class='container mt-5'><div class='alert alert-danger'>Brak dostępu! Musisz być administratorem.</div></div>");
+}
