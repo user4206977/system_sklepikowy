956bb8fabfccab3b4a45f089066be25bbdbc7201
Wojciech S <wojciechstrzezik@gmail.com>
2026-05-29 11:49:38 +0200

    Modyfikacja obsługi modyfikacji statusu w bazie dnaych

--- admin/products.php
+++ admin/products.php
@@ -17,3 +17,9 @@ if (isset($_POST['update_status'])) {
     $product_id = $_POST['product_id'];
     $is_promoted = isset($_POST['is_promoted']) ? 1 : 0;
     $is_available = isset($_POST['is_available']) ? 1 : 0;
+    
+    $stmt = $pdo->prepare("UPDATE products SET is_promoted = ?, is_available = ? WHERE id = ?");
+    $stmt->execute([$is_promoted, $is_available, $product_id]);
+    header("Location: products.php");
+    exit;
+}
